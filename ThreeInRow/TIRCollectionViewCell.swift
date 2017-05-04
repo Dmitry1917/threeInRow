b816be3be1946223af5c334f7b1bce52217d7846
DMITRY SINYOV <dmitry@MacBookProDMITRY.local>
2017-05-04 13:37:39 +0300

    Begin learn collectionView.

---
+++ ThreeInRow/TIRCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TIRCollectionViewCell.swift
+//  ThreeInRow
+//
+//  Created by DMITRY SINYOV on 04.05.17.
+//  Copyright © 2017 DMITRY SINYOV. All rights reserved.
+//
+
+import UIKit
+
+class TIRCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var someContentView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
